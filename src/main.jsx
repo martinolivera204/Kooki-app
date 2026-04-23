6c46306711c40b9f4e293a8b038a1959406558ef
martinolivera204 <martinolivera204@gmail.com>
2026-04-23 11:11:57 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import DeTemporada from './DeTemporada';
+import AhorroExpress from './AhorroExpress';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/detemporada" element={<DeTemporada />} />
+        <Route path="/ahorroexpress" element={<AhorroExpress />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
